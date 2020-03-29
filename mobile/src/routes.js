58ad127f83f4b502391eae6eca6766289a102c4d
Adriano Trezub Dea <adriano.trezub@gmail.com>
2020-03-29 02:20:58 -0300

    Backend Validations

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack'
+import { StatusBar } from 'react-native';
 
 import Incidents from './pages/Incidents'
 import Detail from './pages/Detail'
@@ -10,9 +11,24 @@ const AppStack = createStackNavigator();
 export default function Routes() {
     return (
         <NavigationContainer>
-            <AppStack.Navigator screenOptions={{headerShown: false }}>
-                <AppStack.Screen name='Incidents' component={Incidents} />
-                <AppStack.Screen name='Detail' component={Detail} />
+            <StatusBar
+                backgroundColor="#fff"
+                barStyle="dark-content" // Here is where you change the font-color
+            />
+            <AppStack.Navigator
+                screenOptions={{
+                    headerShown: false,
+                    cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                }}
+            >
+                <AppStack.Screen
+                    name='Incidents'
+                    component={Incidents}
+                />
+                <AppStack.Screen
+                    name='Detail'
+                    component={Detail}                    
+                />
             </AppStack.Navigator>
         </NavigationContainer>
     );
